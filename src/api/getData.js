cbb4bfce82b2966d92bc90dafc6fe5f329e181ae
Edwin Moreno <edwinmoreno77@gmail.com>
2024-09-09 13:11:07 -0300

    getCharacterByName function added

--- src/api/getData.js
+++ src/api/getData.js
@@ -35,3 +35,25 @@ export const getPagesRickAndMorty = async (page) => {
         console.log("Error capturado en el fetch", error);
     }
 };
+
+export const getCharacterByName = async (name) => {
+    try {
+        const response = await fetch(
+            `https://rickandmortyapi.com/api/character/?name=${name}`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+        const data = await response.json();
+
+        return data.results;
+    } catch (error) {
+        console.log("Error capturado en el fetch", error);
+    }
+};
