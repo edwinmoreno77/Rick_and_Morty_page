6fc363315caa38b18632438295001b772f995163
Edwin Moreno <edwinmoreno77@gmail.com>
2024-09-29 22:47:25 -0300

    baseUrl added

--- src/api/getData.js
+++ src/api/getData.js
@@ -1,59 +1,52 @@
-export const getCharacterById = async (id) => {
-    try {
-        const response = await fetch(
-            `https://rickandmortyapi.com/api/character/${id}`,
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }
-        );
-        if (!response.ok) {
-            throw new Error(`Error: ${response.status} ${response.statusText}`);
-        }
-        const data = await response.json();
+const baseUrl = "https://rickandmortyapi.com/api";
 
-        return data;
-    } catch (error) {
-        console.log("Error capturado en el fetch", error);
+export const getCharacterById = async (id) => {
+  try {
+    const response = await fetch(`${baseUrl}/character/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
     }
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log("Error capturado en el fetch", error);
+  }
 };
 
-
 export const getPagesRickAndMorty = async (page) => {
-    try {
-        const response = await fetch(
-            `https://rickandmortyapi.com/api/character/?page=${page}`
-        );
-        if (!response.ok) {
-            throw new Error(`Error: ${response.status} ${response.statusText}`);
-        }
-        const data = await response.json();
-        return data.results;
-    } catch (error) {
-        console.log("Error capturado en el fetch", error);
+  try {
+    const response = await fetch(`${baseUrl}/character/?page=${page}`);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
     }
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.log("Error capturado en el fetch", error);
+  }
 };
 
 export const getCharacterByName = async (name) => {
-    try {
-        const response = await fetch(
-            `https://rickandmortyapi.com/api/character/?name=${name}`,
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }
-        );
-        if (!response.ok) {
-            throw new Error(`Error: ${response.status} ${response.statusText}`);
-        }
-        const data = await response.json();
-
-        return data.results;
-    } catch (error) {
-        console.log("Error capturado en el fetch", error);
+  try {
+    const response = await fetch(`${baseUrl}/character/?name=${name}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
     }
+    const data = await response.json();
+
+    return data.results;
+  } catch (error) {
+    console.log("Error capturado en el fetch", error);
+  }
 };
